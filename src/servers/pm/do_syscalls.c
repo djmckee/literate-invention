5efa319809e66f94789588b8c70e0dee9b5f2d47
Dylan McKee <dylan@djmckee.co.uk>
2015-12-16 22:08:26 +0000

    System call 3

--- src/servers/pm/do_syscalls.c
+++ src/servers/pm/do_syscalls.c
@@ -103,8 +103,8 @@ int do_getnchildren() {
         }
     }
 
-    // Print count to the console...
-    printf("Process with id %d has %d children.", pidToList, count);
+    // Print count to the console (just for debugging!)...
+    //printf("Process with id %d has %d children.", pidToList, count);
 
     // Return the count...
     return count;
@@ -113,5 +113,71 @@ int do_getnchildren() {
 
 // Implementation of 3.3 Getting child pids (getchildpids)
 int do_getchildpids() {
-    return 0;
+    // Get the input of process id from the message (m_in is a global message struct variable, get first param. from it)
+    int pidToList = m_in.m1_i1;
+
+    // Get the limit of child processes to find from the message
+    int maximumProcessLimit = m_in.m1_i2;
+
+    int processCount = NR_PROCS;
+    // Process ID must be between 1 and the maximum, perform some bounds checking...
+    if (pidToList < 1 || pidToList >= processCount) {
+        // pid is not valid! Return with error code -1....
+        return -1;
+    }
+
+    // Create a local array of process IDs; as large as the max. number of possible processes (just to be safe).
+    pid_t matchingChildren[processCount];
+
+    // Create counter var (which will also = array size)
+    int count = 0;
+
+    // Iterate the mproc array...
+    for (int i = 0; i < processCount; i++) {
+        // Get current process
+        struct mproc *currentProcess = &mproc[i];
+        // Ensure there's an actual struct there...
+        if (currentProcess != NULL) {
+            // Go ahead and read process ID
+            pid_t processId = currentProcess->mp_pid;
+
+            // Check process isn't just blank (i.e. the ID must be greater than 0)...
+            if (processId > 0) {
+                // It's an actual process... get parent
+                int parentProcessIndex = currentProcess->mp_parent;
+
+                // Get parent from mproc array, at index specified in current process
+                struct mproc *parentProcess = &mproc[parentProcessIndex];
+
+                // Get parent process ID...
+                pid_t parentProcessId = parentProcess->mp_pid;
+
+                // Check if parent process ID matches the one we're looking for...
+                if (parentProcessId == pidToList) {
+                    // It's a child of the process we want to count the children of!
+
+                    // Add PID to the array
+                    matchingChildren[count] = processId;
+
+                    // Increment count...
+                    count++;
+
+                    // If we've hit the limit of child processes that we're meant to find then give up...
+                    if (count == maximumProcessLimit) {
+                        // Break out of the loop - we've already found the desired maximum number of processes
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+
+    // Print count to the console (just for debugging!)...
+    //printf("Process with id %d has %d children.", pidToList, count);
+
+    // TODO: Copy process ID array into the pointer we've been passed
+
+    // Return the count of child processes found...
+    return count;
 }
