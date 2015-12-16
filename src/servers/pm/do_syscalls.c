61812eb9bc0cb8bc056f78d5b6491175e65589f7
Dylan McKee <dylan@djmckee.co.uk>
2015-12-16 21:15:29 +0000

    2nd system call

--- src/servers/pm/do_syscalls.c
+++ src/servers/pm/do_syscalls.c
@@ -59,7 +59,56 @@ int do_printchildpids() {
 
 // Implementation of 3.2 Counting children (getnchildren)
 int do_getnchildren() {
-    return 0;
+    // Get the input of process id from the message (m_in is a global message struct variable, get first param. from it)
+    int pidToList = m_in.m1_i1;
+
+    int processCount = NR_PROCS;
+    // Process ID must be between 1 and the maximum, perform some bounds checking...
+    if (pidToList < 1 || pidToList >= processCount) {
+        // pid is not valid! Return with error code -1....
+        return -1;
+    }
+
+    // Create counter var
+    int count = 0;
+
+    // Iterate the mproc array...
+    for (int i = 0; i < processCount; i++) {
+        // Get current process
+        struct mproc *currentProcess = &mproc[i];
+        // Ensure there's an actual struct there...
+        if (currentProcess != NULL) {
+            // Go ahead and read process ID
+            pid_t processId = currentProcess->mp_pid;
+
+            // Check process isn't just blank (i.e. the ID must be greater than 0)...
+            if (processId > 0) {
+                // It's an actual process... get parent
+                int parentProcessIndex = currentProcess->mp_parent;
+
+                // Get parent from mproc array, at index specified in current process
+                struct mproc *parentProcess = &mproc[parentProcessIndex];
+
+                // Get parent process ID...
+                pid_t parentProcessId = parentProcess->mp_pid;
+
+                // Check if parent process ID matches the one we're looking for...
+                if (parentProcessId == pidToList) {
+                    // It's a child of the process we want to count the children of!
+                    // Increment count...
+                    count++;
+
+                }
+            }
+        }
+    }
+
+    // Print count to the console...
+    printf("Process with id %d has %d children.", pidToList, count);
+
+    // Return the count...
+    return count;
+
 }
 
 // Implementation of 3.3 Getting child pids (getchildpids)
