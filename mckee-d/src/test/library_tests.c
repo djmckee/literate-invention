3a4a842099d83a799bac7af3140a8285dec8041a
Dylan McKee <dylan@djmckee.co.uk>
2015-12-16 23:25:57 +0000

    Tests

--- mckee-d/src/test/library_tests.c
+++ mckee-d/src/test/library_tests.c
@@ -3,16 +3,11 @@
  *  Date: 10/12/15
  *  Purpose: A test of my library functions for the CSC2025 assignment.
  */
- #include <unistd.h>
- #include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-/*
-    A test of the following functions:
-    int printchildpids(pid_t pid);
-    int getnchildren(pid_t pid);
-    int getchildpids(pid_t pid, int nchildren, pid_t *childpids);
-
-*/
+// Define constant test PID
+#define TEST_PID 140
 
 // Test function prototypes
 void testPrintChildPids();
@@ -20,6 +15,7 @@ void testGetNChildren();
 void testGetChildPids();
 
 int main(int argc, char **argv) {
+    printf("Library function tests:\n");
     // Run the test functions
     testPrintChildPids();
     testGetNChildren();
@@ -28,13 +24,44 @@ int main(int argc, char **argv) {
 }
 
 void testPrintChildPids() {
+    int status = printchildpids(TEST_PID);
+
+    if (status == 0) {
+        printf("testPrintChildPids success\n");
+    } else {
+        printf("testPrintChildPids failed\n");
+        // TODO: Print error number/description
+    }
 
 }
 
 void testGetNChildren() {
+    int childrenForProcess = getnchildren(TEST_PID);
+
+    if (childrenForProcess >= 0) {
+        printf("testGetNChildren success\n");
+    } else {
+        printf("testGetNChildren failed\n");
+        // TODO: Print error number/description
+    }
 
 }
 
 void testGetChildPids() {
+    // Define a limit
+    const int limit = 50;
+
+    // Create an array of PIDs to fill
+    pid_t *pidArray[limit] = malloc(sizeof(pid_t) * limit);
+
+    // Test...
+    int numberOfChildren = getchildpids(TEST_PID, limit, pidArray);
+
+    if (numberOfChildren >= 0) {
+        printf("testGetChildPids success\n");
+    } else {
+        printf("testGetChildPids failed\n");
+        // TODO: Print error number/description
+    }
 
 }
---
+++ mckee-d/src/test/syscall_tests.c
@@ -0,0 +1,74 @@
+/*
+ *  Author: Dylan McKee (140357185)
+ *  Date: 10/12/15
+ *  Purpose: A test of my system call functions for the CSC2025 assignment.
+ */
+#include <unistd.h>
+#include <stdlib.h>
+#include <lib.h>
+
+// Define constant test PID
+#define TEST_PID 140
+
+// Test function prototypes
+void testPrintChildPids();
+void testGetNChildren();
+void testGetChildPids();
+
+int main(int argc, char **argv) {
+    printf("System call tests:\n");
+    // Run the test functions
+    testPrintChildPids();
+    testGetNChildren();
+    testGetChildPids();
+
+}
+
+void testPrintChildPids() {
+    message m;
+    m.m1_i1 = TEST_PID;
+
+    int result = _syscall(PM_PROC_NR, PRINTCHILDPIDS, &m);
+
+    if (result == 0) {
+        printf("PRINTCHILDPIDS system call success\n");
+    } else {
+        printf("PRINTCHILDPIDS system call failed\n");
+    }
+
+}
+
+void testGetNChildren() {
+    message m;
+    m.m1_i1 = TEST_PID;
+
+    int result = _syscall(PM_PROC_NR, GETNCHILDREN, &m);
+
+
+    if (result == 0) {
+        printf("GETNCHILDREN system call success\n");
+    } else {
+        printf("GETNCHILDREN system call failed\n");
+    }
+
+}
+
+void testGetChildPids() {
+    // Define a limit
+    const int limit = 50;
+
+    message m;
+    m.m1_i1 = TEST_PID;
+    m.m1_m2 = limit;
+
+    // TODO: add in array somehow!!?
+
+    int result = _syscall(PM_PROC_NR, GETCHILDPIDS, &m);
+
+    if (result == 0) {
+        printf("GETCHILDPIDS system call success\n");
+    } else {
+        printf("GETCHILDPIDS system call failed\n");
+    }
+
+}
